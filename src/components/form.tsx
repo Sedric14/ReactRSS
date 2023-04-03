2587841a4c13222f8a0dd5d3d318495ad929f34d
Sedric <sedricgreen6@gmail.com>
2023-04-03 23:55:28 +0300

    fix: remove Yup validation

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -11,28 +11,91 @@ const Form = (props: RefTypes) => {
       <form className="form" onSubmit={props.handle} data-testid="form">
         <label className="label">
           Name:
-          <input className="inputName" type="text" {...props.register('name')} />
+          <input
+            className="inputName"
+            type="text"
+            {...props.register('name', {
+              required: {
+                value: true,
+                message: 'Name is required',
+              },
+              pattern: {
+                value: /^[A-Z,А-Я]/,
+                message: 'Name must be start with uppercase',
+              },
+              maxLength: {
+                value: 20,
+                message: 'Name must not exceed 20 characters',
+              },
+              minLength: {
+                value: 2,
+                message: 'Name must be at least 2 characters',
+              },
+            })}
+          />
         </label>
         <p className={`form-control ${props.errors.name ? 'errDis' : 'errMess'}`}>
           {props.errors.name?.message}
         </p>
         <label className="label">
           Surname:
-          <input className="inputSurname" type="text" {...props.register('surname')} />
+          <input
+            className="inputSurname"
+            type="text"
+            {...props.register('surname', {
+              required: {
+                value: true,
+                message: 'Surname is required',
+              },
+              pattern: {
+                value: /^[A-Z,А-Я]/,
+                message: 'Surname must be start with uppercase',
+              },
+              maxLength: {
+                value: 20,
+                message: 'Surname must not exceed 20 characters',
+              },
+              minLength: {
+                value: 2,
+                message: 'Surname must be at least 2 characters',
+              },
+            })}
+          />
         </label>
         <p className={`form-control ${props.errors.surname ? 'errDis' : 'errMess'}`}>
           {props.errors.surname?.message}
         </p>
         <label className="label">
           Birthday:
-          <input className="inputBirthday" type="date" {...props.register('date')} />
+          <input
+            className="inputBirthday"
+            type="date"
+            {...props.register('date', {
+              required: {
+                value: true,
+                message: 'You must enter date',
+              },
+            })}
+          />
         </label>
         <p className={`form-control ${props.errors.date ? 'errDis' : 'errMess'}`}>
           {props.errors.date?.message}
         </p>
         <label className="label">
           Country:
-          <select className="select" {...props.register('country')}>
+          <select
+            className="select"
+            {...props.register('country', {
+              required: {
+                value: true,
+                message: 'Choose your country',
+              },
+              pattern: {
+                value: /[^empty]/,
+                message: 'Please choose your country',
+              },
+            })}
+          >
             <option value="empty">Choose your country</option>
             <option value="Ukraine">Ukraine</option>
             <option value="Belarus">Belarus</option>
@@ -45,7 +108,16 @@ const Form = (props: RefTypes) => {
         </p>
         <label className="label">
           I consent to my personal data:
-          <input className="inputCheckbox" type="checkbox" {...props.register('check')} />
+          <input
+            className="inputCheckbox"
+            type="checkbox"
+            {...props.register('check', {
+              required: {
+                value: true,
+                message: 'Check to submit the form is required',
+              },
+            })}
+          />
         </label>
         <p className={`form-control ${props.errors.check ? 'errDis' : 'errMess'}`}>
           {props.errors.check?.message}
@@ -60,7 +132,12 @@ const Form = (props: RefTypes) => {
             className="inputGender"
             type="radio"
             value="female"
-            {...props.register('gender')}
+            {...props.register('gender', {
+              required: {
+                value: true,
+                message: 'Choose your gender',
+              },
+            })}
           />
         </label>
         <p className={`form-control ${props.errors.gender ? 'errDis' : 'errMess'}`}>
@@ -72,8 +149,14 @@ const Form = (props: RefTypes) => {
             className="inputFile"
             type="file"
             accept="image/*"
-            {...props.register('file')}
+            {...props.register('file', {
+              required: {
+                value: true,
+                message: 'Choose avatar',
+              },
+            })}
             onChange={(e) => {
+              if (props.errors.file) props.errors.file.message = '';
               if (e.target?.files?.[0]) {
                 sessionStorage.setItem('img', URL.createObjectURL(e.target?.files?.[0]));
               }
@@ -81,7 +164,7 @@ const Form = (props: RefTypes) => {
           />
         </label>
         <p className={`form-control ${props.errors.file ? 'errDis' : 'errMess'}`}>
-          Choose an image
+          {props.errors.file?.message}
         </p>
         <input className="submit" type="submit" value="Отправить" />
       </form>
--- src/pages/formsPage.tsx
+++ src/pages/formsPage.tsx
@@ -4,38 +4,14 @@ import FormCard from 'components/formCard';
 import Form from 'components/form';
 import { useForm } from 'react-hook-form';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { yupResolver } from '@hookform/resolvers/yup';
-import * as Yup from 'yup';
 
 const FormPage: React.FC = () => {
-  const validationSchema = Yup.object().shape({
-    name: Yup.string()
-      .required('Name is required')
-      .matches(/^[A-Z,А-Я]/, 'Name must be start with uppercase')
-      .min(2, 'Username must be at least 6 characters')
-      .max(20, 'Username must not exceed 20 characters'),
-    surname: Yup.string()
-      .required('Username is required')
-      .matches(/^[A-Z,А-Я]/, 'Name must be start with uppercase')
-      .min(2, 'Username must be at least 6 characters')
-      .max(20, 'Username must not exceed 20 characters'),
-    date: Yup.string().required('Enter date'),
-    check: Yup.bool().oneOf([true], 'Check to submit the form is required'),
-    country: Yup.string()
-      .required('Choose your country')
-      .matches(/[^empty]/, 'Please choose your country'),
-    gender: Yup.string().matches(/^male|female/, 'Choose your gender'),
-  });
-
   const {
     register,
     handleSubmit,
     reset,
     formState: { errors },
-  } = useForm<FormFields>({
-    resolver: yupResolver(validationSchema),
-  });
-
+  } = useForm<FormFields>();
   const onSubmit = (data: FormFields) => {
     data.file = sessionStorage.getItem('img') as string;
     changeArr(arr.concat(data));
