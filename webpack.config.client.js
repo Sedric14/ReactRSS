d1ce5c78904100d63cbfc8ddbe0969ff24d70804
Sedric <sedricgreen6@gmail.com>
2023-04-18 18:35:49 +0300

    feat: first attempt

--- webpack.config.client.js
+++ webpack.config.client.js
@@ -1,32 +1,46 @@
+const webpack = require('webpack');
+const MiniCssExtactPlugin = require('mini-css-extact-plugin');
+const config = require('./webpack.common').createConfig({
+  target: 'client',
+});
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
 
 module.exports = {
-  name: 'client',
-  entry: {
-    client: path.resolve(__dirname, 'src/main.tsx'),
-  },
-  mode: 'production',
-  output: {
-    path: path.resolve(__dirname + '/dist/static'),
-    filename: '[name].[contenthash].js',
-    publicPath: '',
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js'],
-  },
-  target: 'web',
+  ...config,
+  // name: 'client',
+  // entry: {
+  //   client: path.resolve(__dirname, 'src/main.tsx'),
+  // },
+  // mode: 'production',
+  // output: {
+  //   path: path.resolve(__dirname + '/dist/static'),
+  //   filename: '[name].[contenthash].js',
+  //   publicPath: '',
+  // },
+  // resolve: {
+  //   extensions: ['.ts', '.tsx', '.js'],
+  // },
+  // target: 'web',
   module: {
+    ...config.module,
     rules: [
+      ...config.module.rules,
       {
-        test: /\.tsx?$/,
-        loader: 'ts-loader',
-        options: {
-          configFile: 'tsconfig.client.json',
-        },
+        test: /\.css$/,
+        use: [MiniCssExtactPlugin.loader, 'css-loader'],
+        // loader: 'ts-loader',
+        // options: {
+        //   configFile: 'tsconfig.client.json',
+        // },
       },
     ],
   },
-  plugins: [new CleanWebpackPlugin(), new WebpackManifestPlugin()],
+  plugins: [
+    ...config.plugins,
+    new MiniCssExtactPlugin(),
+    new CleanWebpackPlugin(),
+    new WebpackManifestPlugin(),
+  ],
 };
--- webpack.config.server.js
+++ webpack.config.server.js
@@ -1,39 +1,46 @@
-const nodeExternals = require('webpack-node-externals');
-const path = require('path');
-const CopyPlugin = require('copy-webpack-plugin');
+// const nodeExternals = require('webpack-node-externals');
+const config = require('./webpack.common').createConfig({
+  target: 'server',
+});
+// const path = require('path');
+// const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  name: 'server',
-  entry: {
-    server: path.resolve(__dirname, 'server/server.tsx'),
-  },
-  mode: 'production',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js',
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx'],
-  },
-  externals: [nodeExternals()],
-  target: 'node',
-  node: {
-    __dirname: false,
-  },
+  ...config,
+  // name: 'server',
+  // entry: {
+  //   server: path.resolve(__dirname, 'server/server.tsx'),
+  // },
+  // mode: 'production',
+  // output: {
+  //   path: path.resolve(__dirname, 'dist'),
+  //   filename: '[name].js',
+  // },
+  // resolve: {
+  //   extensions: ['.ts', '.tsx'],
+  // },
+  // externals: [nodeExternals()],
+  // target: 'node',
+  // node: {
+  //   __dirname: false,
+  // },
   module: {
+    ...config.module,
     rules: [
+      ...config.module.rules,
       {
-        test: /\.tsx?$/,
-        loader: 'ts-loader',
-        options: {
-          configFile: 'tsconfig.server.json',
-        },
+        test: /\.css$/,
+        use: 'null-loader',
+        // loader: 'ts-loader',
+        // options: {
+        //   configFile: 'tsconfig.server.json',
+        // },
       },
     ],
   },
-  plugins: [
-    new CopyPlugin({
-      patterns: [{ context: 'server', from: 'views', to: 'views' }],
-    }),
-  ],
+  // plugins: [
+  //   new CopyPlugin({
+  //     patterns: [{ context: 'server', from: 'views', to: 'views' }],
+  //   }),
+  // ],
 };
